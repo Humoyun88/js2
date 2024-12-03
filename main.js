65210c0836fa49182df754e9d1f331cbf7ebaeb2
humoyun <khumo1288@gmail.com>
2024-12-03 17:09:35 +0300

    js2

---
+++ main.js
@@ -0,0 +1,7 @@
+var Humoyun1 = prompt('birinchi sonni kiriting')
+console.log(Humoyun1);
+var Humoyun2 = prompt('ikkinchi sonni kiriting')
+console.log(Humoyun2); 
+var Humoyun3 = prompt('uchinchi sonni kiriting')
+console.log(Humoyun3);
+alert('Среднее арифметическое ' + Humoyun1+Humoyun2+Humoyun3/3)
